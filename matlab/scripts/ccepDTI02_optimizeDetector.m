6296338747acd5dcc749071f825aa28fef2aa4d6
Susanne Jelsma <susanne@bird.umcutrecht.nl>
2021-11-18 09:43:45 +0100

    kappa score between scorers and load data of visual scorers

--- matlab/scripts/ccepDTI02_optimizeDetector.m
+++ matlab/scripts/ccepDTI02_optimizeDetector.m
@@ -23,6 +23,14 @@ myDataPath = setLocalDataPath(cfg);
 % I think that we should first mention which patients have visually scored
 % CCEPs in sEEG data, and load these specific patients
 
+sub_label = input('Patient number (RESPXXXX) (select multiple patients by separating each with a comma): ','s'); %(select multiple patients by separating each with a comma)
+
+cfg.sub_label = strsplit(sub_label,{', ',','});
+
+cfg = selectPatients(cfg, myDataPath);
+%% load visual scoring results
+dataBase_visualScores = load_visualScores(myDataPath,cfg,1);
+dataBase_visualScores2 = load_visualScores(myDataPath,cfg,2);
 %% load ECoGs 
 
 dataBase = load_ECoGdata(myDataPath,cfg);
@@ -78,6 +86,30 @@ else
     disp('Data is not re-referenced and all trials of one stimulus pair are averaged.')
 end
 
+%% kappa score
+
+% pre-allocation
+TP = NaN(1); % true positives
+FN = NaN(1); % false negatives
+FP = NaN(1); % false positives
+TN = NaN(1); % true negatives   
+kappa= NaN(1); % Cohen's kappa
+
+    for subj = 1:size(dataBase_visualScores,2)
+        for run = 1:size(dataBase_visualScores(subj).metadata,2)
+            scored = dataBase_visualScores(subj).metadata(run).ccep.checked;
+            scored2 = dataBase_visualScores2(subj).metadata(run).ccep.checked;
+            TP(subj,run) = numel(find( scored2 == 1 & scored == 1));
+            FN(subj,run) = numel(find( scored2 == 1 & scored == 0));
+            FP(subj,run) = numel(find( scored2 == 0 & scored == 1));
+            TN(subj,run) = numel(find( scored2 == 0 & scored == 0));
+
+            kappa(subj,run)= (2*(TP(subj,run)*TN(subj,run)-FN(subj,run)*FP(subj,run)))...
+                /((TP(subj,run)+FP(subj,run))*(FP(subj,run)+TN(subj,run))+(TP(subj,run)+FN(subj,run))*(FN(subj,run)+TN(subj,run))) ;
+        end
+    end
+   
+
 %% optimize detector
 
 % for cECoG data, these are the best parameters:
@@ -85,7 +117,7 @@ cfg.amplitude_thresh = 2.6;
 cfg.n1_peak_range = 100;
 
 % let's find out what are the best parameters for sEEG data
-% net to the amplitude_threshold, you might want to optimize pre_stim_sd
+% next to the amplitude_threshold, you might want to optimize pre_stim_sd
 % (line121), and sel (line 145).
 % and now, only the negative peaks are taken into account, but I think,
 % since sEEG is stimulated differently, that you might want to take both
@@ -105,17 +137,15 @@ for amplTh = 0.5:0.5:5 % vary amplitude threshold
 
     for subj = 1:size(dataBase,2)
         for run = 1:size(dataBase(subj).metadata,2)
-            detected = dataBase(subj).metadata(run).ccep.n1_peak_sample;
+            detected = dataBase(subj).metadata(run).ccep.n1_peak_sample; %nan als geen peak gevonden, dan dus 0
             detected(~isnan(detected)) = 1;
+            detected(isnan(detected)) = 0;
+            scored = dataBase_visualScores(subj).metadata(run).ccep.checked;
 
-            TP(subj,run,n) = numel(find(dataBase(subj).metadata(run).ccep.checked == 1 & ...
-                detected == 1));
-            FN(subj,run,n) = numel(find(dataBase(subj).metadata(run).ccep.checked == 1 & ...
-                detected == 0));
-            FP(subj,run,n) = numel(find(dataBase(subj).metadata(run).ccep.checked == 0 & ...
-                detected == 1));
-            TN(subj,run,n) = numel(find(dataBase(subj).metadata(run).ccep.checked == 0 & ...
-                detected == 0));
+            TP(subj,run,n) = numel(find( scored == 1 & detected == 1));
+            FN(subj,run,n) = numel(find( scored == 1 & detected == 0));
+            FP(subj,run,n) = numel(find( scored == 0 & detected == 1));
+            TN(subj,run,n) = numel(find( scored == 0 & detected == 0));
         end
     end
     n=n+1;
