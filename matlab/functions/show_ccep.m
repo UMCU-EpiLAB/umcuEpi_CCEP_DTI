410cc64d6145d27a8b282a756f4f8a5777aec81b
Susanne Jelsma <susanne@bird.umcutrecht.nl>
2021-12-10 11:32:06 +0100

    fix mistake in the function to show and re-score falsely detected cceps

--- matlab/functions/show_ccep.m
+++ matlab/functions/show_ccep.m
@@ -1,9 +1,8 @@
-function show_ccep(dataBase,F_stimp,F_chan,subj,run,cfg)
-
+function data=show_ccep(dataBase,F_stimp,F_chan,subj,run,cfg)
+data = zeros(1);
 for i = 1:length(F_stimp)
-    for j = 1:length(F_chan)
             stimp = F_stimp(i);
-            chan = F_chan(j);
+            chan = F_chan(i);
             fs = dataBase(subj).metadata(run).ccep_header.Fs;
             tt = -cfg.epoch_prestim+1/fs:1/fs:cfg.epoch_length-cfg.epoch_prestim;
             H=figure(3);
@@ -98,7 +97,20 @@ for i = 1:length(F_stimp)
                 xlabel('Time (s)')
                 ylabel('Amplitude (uV)')
                 title('Zoomed average signal')
-            uiwait(msgbox('CCEP viewed?'));
+
+            perc = i / length(F_stimp) *100;
+            x = input(sprintf('%2.1f %% --- stimpair = %s-%s chan = %s --- Is this a CCEP? (y/n): ',...
+                    perc,dataBase(subj).metadata(run).cc_stimchans{stimp,:},dataBase(subj).metadata(run).ch{chan}),'s');
+            % input if it is indeed a FP/FN or not by scoring the CCEPs
+            % again
+
+            if strcmp(x,'y') 
+                    data(chan,stimp) = 1 ;
+            else
+                    data(chan,stimp) = 0 ;
+            end
             close(H)
-    end
+            clear('x')
+           
 end
+fprintf('Visual rating of %s completed\n',dataBase(subj).sub_label)
