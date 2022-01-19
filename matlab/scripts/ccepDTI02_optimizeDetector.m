e44f629331126901025526983d684825fe36d959
Susanne Jelsma <susanne@stein.umcutrecht.nl>
2022-01-19 12:39:15 +0100

    change way of selecting subjects to use and replace for loop by more efficient script in merge score of two scorers and include best combination for d_roc and Fscore and some other performance calculations

--- matlab/scripts/ccepDTI02_optimizeDetector.m
+++ matlab/scripts/ccepDTI02_optimizeDetector.m
@@ -1,15 +1,12 @@
 % ccepDTI02_optimizeDetector
+% determination of optimal setting for n1-detector for sEEG data
 
 % author: Dorien van Blooijs & Susanne Jelsma
 % date: October 2021
 
-% make sure you first have some visually scored CCEPs (ccepDTI01_scoreCCEP)
-% before you continue with this script
-
 % load ECoG data, split into stimulation trials, re-reference the data,
-% 
-
-
+% load scoring, calculate kappa, 
+% SUMMARY
 %% set paths
 % set umcuEpi_CCEP_DTI/matlab in your directory and run this section
 
@@ -19,18 +16,37 @@ cfg.folderinput = 'chronic_ECoG'; % from which folder would you like to load ECo
 myDataPath = setLocalDataPath(cfg);
 
 %% patient characteristics
+%instead of these two lines search automaticly for the visual scored patients
+    %sub_label = input('Patient number (RESPXXXX) (select multiple patients by separating each with a comma): ','s'); %(select multiple patients by separating each with a comma)
+    %cfg.sub_label = strsplit(sub_label,{', ',','});
 
-% I think that we should first mention which patients have visually scored
-% CCEPs in sEEG data, and load these specific patients
+files1 = dir(myDataPath.CCEPpath); files2 = dir(myDataPath.CCEPpath2);
+files1(1:2) = []; files2(1:2) = [];
+sub_label1 = cell(1,size(files1,1));
+for subj=1:size(files1,1)
+sub_label1(1,subj)= cellstr(erase(files1(subj).name,'sub-'));
+end
+sub_label2 = cell(1,size(files1,1));
+for subj=1:size(files2,1)
+sub_label2(1,subj)= cellstr(erase(files2(subj).name,'sub-'));
+end
+sub_label = intersect(sub_label1,sub_label2);
 
-sub_label = input('Patient number (RESPXXXX) (select multiple patients by separating each with a comma): ','s'); %(select multiple patients by separating each with a comma)
-
-cfg.sub_label = strsplit(sub_label,{', ',','});
+i = 1;
+for subj= 1:size(sub_label,2)
+    x = input(sprintf('Load subject %s? (y/n): ',char(sub_label(subj))),'s');       
+    if strcmp(x,'y') 
+                    cfg.sub_label(1,i) = sub_label(subj) ;
+                    i=i+1;
+    end
+end
 
 cfg = selectPatients(cfg, myDataPath);
+
 %% load visual scoring results
-dataBase_visualScores = load_visualScores(myDataPath,cfg,1);
-dataBase_visualScores2 = load_visualScores(myDataPath,cfg,2);
+
+dataBase_visualScores = load_visualScores(myDataPath,cfg);
+
 %% load ECoGs 
 
 dataBase = load_ECoGdata(myDataPath,cfg);
@@ -38,6 +54,7 @@ dataBase = load_ECoGdata(myDataPath,cfg);
 disp('All ECoGs are loaded')
 
 %% preprocessing CCEP in ECoG
+
 clear cfg
 
 % preprocessing step
@@ -87,6 +104,7 @@ else
 end
 
 %% kappa score
+
 % pre-allocation
 TP = NaN(1); % true positives
 FN = NaN(1); % false negatives
@@ -106,6 +124,7 @@ kappa_score = NaN(1); % Cohen's kappa
                 /((TP(subj,run)+FP(subj,run))*(FP(subj,run)+TN(subj,run))+(TP(subj,run)+FN(subj,run))*(FN(subj,run)+TN(subj,run))) ;
         end
     end
+
 % with the external function kappa
 kappa_score2 = NaN(1); % Cohen's kappa via function
 
@@ -122,27 +141,21 @@ kappa_score2 = NaN(1); % Cohen's kappa via function
             clear('confusion_matrix');
         end
     end
+
 %% merge scores of two scorers
 % only the scored CCEPs we both agreed on are officially visual scored
     for subj = 1:size(dataBase_visualScores,2)
         for run = 1:size(dataBase_visualScores(subj).metadata,2)
             scored = dataBase_visualScores(subj).metadata(run).ccep.checked;
-            scored2 = dataBase_visualScores2(subj).metadata(run).ccep.checked;
-            [ccep_row, ccep_col] = find( scored == 1 & scored2 == 1);
-            agreed_cceps = zeros(size(scored));
-            for indx=1:length(ccep_row)
-            agreed_cceps(ccep_row(indx),ccep_col(indx)) = 1;
-            end
-            dataBase_visualScores(subj).metadata(run).visual_scored = agreed_cceps;
+            scored2 = dataBase_visualScores(subj).metadata(run).ccep2.checked;       
+            scored_both = scored + scored2;
+            scored_both(scored_both == 1) = 0; % when there is disagreement, value is 1, this should be regarded as no CCEP
+            scored_both(scored_both == 2) = 1; % when there is agreement, value is 2, this should be regarded as a CCEP
+            dataBase_visualScores(subj).metadata(run).visual_scored = scored_both;
         end
     end
-clear('agreed_cceps');
-clear('ccep_row');
-clear('ccep_col');
-clear('indx');
-clear('scored');
-clear('scored2');
-clear('dataBase_visualScores2');
+
+clear scored_both scored scored2 dataBase_visualScores2
 %% optimize detector
 % for cECoG data, these are the best parameters:
 cfg.amplitude_thresh = 2.6;
@@ -158,9 +171,9 @@ FP = NaN(1); % false positives
 TN = NaN(1); % true negatives
 %combination = NaN(1); % combination of parameters. dit geeft een error
 
-for sd = 0:10:100 %10:1:50
-for sl = 10:10:200 %10:1:20
-for amplTh = 0.5:0.5:15 %1.5:0.1:2.5 
+for sd = 11:1:13 
+for sl = 10:1:12 
+for amplTh = 3.2:0.1:3.3 
     cfg.amplitude_thresh = amplTh;
     cfg.minSD = sd;
     cfg.sel = sl;
@@ -177,15 +190,18 @@ for amplTh = 0.5:0.5:15 %1.5:0.1:2.5
             FN(subj,run,n) = numel(find( scored == 1 & detected == 0));
             FP(subj,run,n) = numel(find( scored == 0 & detected == 1));
             TN(subj,run,n) = numel(find( scored == 0 & detected == 0));
+            
         end
     end
     n=n+1;
-    
+    fprintf('...Combination: Amplitude Threshold=%s, min SD of baseline=%s, Sel=%s, has been run...\n',...
+        amplTh,sd,sl)
 end
 end
 end
 
 %% calculate and visualize performance for each setting
+
 % combine the performances of each subj and run.
 TP_all = NaN(1);
 FN_all = NaN(1);
@@ -197,9 +213,10 @@ ppv = NaN(1); % positive predictive value
 npv = NaN(1); % negative predicitive value
 d_roc = NaN(1); % distance to top left corner of ROC 
 d_prc = NaN(1); % distance to top left corner of PRC (precision-recall curve)
+F_score = NaN(1); % F-score (wss hetzelfde als d_roc)
 
         for n = 1:length(combination)
-        TP_all(n,1) = (sum(nonzeros(TP(:,:,n))));
+        TP_all(n,1) = sum(nonzeros(TP(:,:,n)));
         FN_all(n,1) = sum(nonzeros(FN(:,:,n)));
         FP_all(n,1) = sum(nonzeros(FP(:,:,n)));
         TN_all(n,1) = sum(nonzeros(TN(:,:,n)));
@@ -208,7 +225,8 @@ d_prc = NaN(1); % distance to top left corner of PRC (precision-recall curve)
         ppv(n,1) = TP_all(n)/(TP_all(n)+FP_all(n));
         npv(n,1) = TN_all(n)/(TN_all(n)+FN_all(n));
         d_roc(n,1) = sqrt((1-sens(n))^2+ (1-spec(n))^2);
-        d_prc(n,1) = sqrt((1-sens(n))^2+ (1-ppv(n))^2);       
+        d_prc(n,1) = sqrt((1-sens(n))^2+ (1-ppv(n))^2);  
+        F_score(n,1) = TP_all(n)/(TP_all(n)+0.5*(FP_all(n)+FN_all(n)));
         end
 
 % analyze the performances per subject
@@ -243,7 +261,19 @@ d_prc_subj = NaN(1); % distance to top left corner of PRC (precision-recall curv
 best_amplTh = combination(best_comb,1);
 best_minSD = combination(best_comb,2);
 best_sel = combination(best_comb,3);
-
+%% more sensitive algorithm
+% best combination for this run
+[value2, best_comb2] = min(d_roc);
+best_amplTh2 = combination(best_comb2,1);
+best_minSD2 = combination(best_comb2,2);
+best_sel2 = combination(best_comb2,3);
+%% algorithm based on fscore
+% best combination for this run
+[value3, best_comb3] = min(F_score);
+best_amplTh3 = combination(best_comb3,1);
+best_minSD3 = combination(best_comb3,2);
+best_sel3 = combination(best_comb3,3);
+%%
 % plot ROC and PRC
 f1 = figure(1);
 scatter(1-spec,sens, 30, [0 0.4470 0.7410], '.')
@@ -271,29 +301,79 @@ ylim([0,1])
 set(gca, 'YTick',ticks, 'XTick', ticks);
 box on
 print(f2,'-dpng', 'PRC_5','-r300')
+
+%% test prut (kan er later uit)
+best_FP = FP_all(best_comb2,1);
+best_FN = FN_all(best_comb2,1);
+best_FP_subj = FP_subj(best_comb2,:);
+best_FN_subj = FN_subj(best_comb2,:);
+all_subj = FN_subj(best_comb,:)+FP_subj(best_comb,:)+TN_subj(best_comb,:)+TP_subj(best_comb,:);
+FN_norm_subj =best_FN_subj./all_subj;
+FP_norm_subj =best_FP_subj./all_subj;
 %% overview of performance values of best combination for this run
 matrix = zeros(6,4);
+
 matrix(1,2:4) = spec_subj(best_comb,:);
 matrix(2,2:4) =  sens_subj(best_comb,:);
 matrix(3,2:4) =  ppv_subj(best_comb,:);
 matrix(4,2:4) =  npv_subj(best_comb,:);
 matrix(5,2:4) =  d_roc_subj(best_comb,:);
 matrix(6,2:4) =  d_prc_subj(best_comb,:);
+
 matrix(1,1) = spec(best_comb,:);
 matrix(2,1) =  sens(best_comb,:);
 matrix(3,1) =  ppv(best_comb,:);
 matrix(4,1) =  npv(best_comb,:);
 matrix(5,1) =  d_roc(best_comb,:);
 matrix(6,1) =  d_prc(best_comb,:);
+
 matrix = matrix.*100;
 matrix = round(matrix);
+
+%%
+matrix2 = zeros(6,4);
+
+matrix2(1,2:4) = spec_subj(best_comb2,:);
+matrix2(2,2:4) =  sens_subj(best_comb2,:);
+matrix2(3,2:4) =  ppv_subj(best_comb2,:);
+matrix2(4,2:4) =  npv_subj(best_comb2,:);
+matrix2(5,2:4) =  d_roc_subj(best_comb2,:);
+matrix2(6,2:4) =  d_prc_subj(best_comb2,:);
+
+matrix2(1,1) = spec(best_comb2,:);
+matrix2(2,1) =  sens(best_comb2,:);
+matrix2(3,1) =  ppv(best_comb2,:);
+matrix2(4,1) =  npv(best_comb2,:);
+matrix2(5,1) =  d_roc(best_comb2,:);
+matrix2(6,1) =  d_prc(best_comb2,:);
+
+matrix2 = matrix2.*100;
+matrix2 = round(matrix2);
+%%
+
+best_spec_subj = spec_subj(best_comb,:);
+best_sens_subj =  sens_subj(best_comb,:);
+best_ppv_subj =  ppv_subj(best_comb,:);
+best_npv_subj =  npv_subj(best_comb,:);
+best_d_roc_subj =  d_roc_subj(best_comb,:);
+best_d_prc_subj =  d_prc_subj(best_comb,:);
+
+best_spec = spec(best_comb,:);
+best_sens =  sens(best_comb,:);
+best_ppv =  ppv(best_comb,:);
+best_npv =  npv(best_comb,:);
+best_d_roc =  d_roc(best_comb,:);
+best_d_prc =  d_prc(best_comb,:);
+
+
 %% show falsely detected cceps
 % plot false positives 
 cfg.amplitude_thresh = best_amplTh;
 cfg.minSD = best_minSD;
 cfg.sel = best_sel;
 dataBase = detect_n1peak_ccep(dataBase, cfg);
-per_CCEP_FP = NaN(1); % percentage of the FP wich is by second observation TP
+CCEP_FP = NaN(1); % number of FP a CCEP by second observation
+per_CCEP_FP = NaN(1);
 for subj = 1:size(dataBase,2)
     for run = 1:size(dataBase(subj).metadata,2)
             detected = dataBase(subj).metadata(run).ccep.n1_peak_sample; 
@@ -303,13 +383,15 @@ for subj = 1:size(dataBase,2)
             [FP_chan,FP_stimp]= find( scored == 0 & detected == 1);
             data_FP = show_ccep(dataBase,FP_stimp,FP_chan,subj,run,cfg);
             dataBase(subj).metadata(run).ccep.n1_peak_sample_FP = data_FP;
-            per_CCEP_FP(subj,run) = sum(data_FP(:)==1)/length(FP_chan)*100;
+            CCEP_FP(subj,run) = sum(data_FP(:)==1); % de CCEPs die eigenlijk wel TP zijn
+            per_CCEP_FP(subj,run) = CCEP_FP(subj,run)/length(FP_chan)*100;
             clear('data_FP')
     end
 end
 %%
 % plot false negatives
-per_CCEP_FN = NaN(1); % percentage of the FN wich is by second observation TN
+CCEP_FN = NaN(1); % number of FP a CCEP by second observation
+per_CCEP_FN = NaN(1);
 for subj = 1:size(dataBase,2)
     for run = 1:size(dataBase(subj).metadata,2)
             detected = dataBase(subj).metadata(run).ccep.n1_peak_sample; 
@@ -319,11 +401,42 @@ for subj = 1:size(dataBase,2)
             [FN_chan,FN_stimp]= find( scored == 1 & detected == 0); 
             data_FN = show_ccep(dataBase,FN_stimp,FN_chan,subj,run,cfg);
             dataBase(subj).metadata(run).ccep.n1_peak_sample_FN = data_FN;
-            per_CCEP_FN(subj,run) = (1-sum(data_FN(:)==1)/length(FN_chan))*100;
+            CCEP_FN(subj,run) = sum(data_FN(:)==1); % de CCEPs die echt FN zijn
+            per_CCEP_FN(subj,run) = CCEP_FN(subj,run)/length(FN_chan)*100;
             clear('data_FN')
     end
 end
+
+%% peakfinder step for step
+cfg.amplitude_thresh = 3.8;
+cfg.minSD = 18;
+cfg.sel = 14;
+dataBase = detect_n1peak_ccep(dataBase, cfg);
+
+amplitude_thresh = cfg.amplitude_thresh;
+n1_peak_range = cfg.n1_peak_range;
+epoch_prestim = cfg.epoch_prestim;
+epoch_length = cfg.epoch_length;
+minSD = cfg.minSD;
+sel = cfg.sel;
+
+subj=1;
+run=1;
+signal = dataBase(subj).metadata(run).cc_epoch_sorted_reref_avg;
+chan=39;
+stimp=2;
+% create time struct 
+tt = (1:epoch_length*dataBase(subj).metadata(run).ccep_header.Fs) / ...
+      dataBase(subj).metadata(run).ccep_header.Fs - epoch_prestim;
+               
+% baseline subtraction: take median of part of the averaged signal for
+% this stimulation pair before stimulation, which is the half of the
+% epoch                
+baseline_tt = tt>-2 & tt<-.1;
+signal_median = median(signal(chan,stimp,baseline_tt),3,'omitnan');
+% subtract median baseline from signal
+new_signal = squeeze(signal(chan,stimp,:)) - signal_median;
+peakfinder(new_signal(find(tt>0,1):find(tt>0.5,1)),sel,[],-1); % negative peaks
+peakfinder(new_signal(find(tt>0,1):find(tt>0.5,1)),sel,[],1); % positive peaks
 %% determine optimal settings
-
-
 
