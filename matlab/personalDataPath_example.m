f2e104382a7f741d2615fbd16356dfab48106cc0
Susanne Jelsma <susanne@ein.umcutrecht.nl>
2023-08-16 17:00:22 +0200

    added comments such that it is clear what you need to fill in

--- matlab/personalDataPath_example.m
+++ matlab/personalDataPath_example.m
@@ -10,10 +10,15 @@ if ~isempty(varargin{1})
     if isstruct(varargin{1})
         if sum(contains(fieldnames(varargin{1}),'folderinput'))
             if contains(varargin{1}.folderinput,'chronic_ECoG')
-                % RESPect database
-                localDataPath.CCEPpath = 'blabla/derivatives/CCEP/';
-                localDataPath.CCEPpath2 = 'blabla/derivatives/CCEP/'; % second observer
-                localDataPath.dataPath = '/blabla/chronic_ECoG/';
+                % CCEP
+                localDataPath.CCEPpath = 'blabla/derivatives/CCEP/'; % path to the visual scored CCEP data
+                localDataPath.CCEPpath2 = 'blabla/derivatives/CCEP/'; % path to the visual scored CCEP data for the second observer (if used)
+                localDataPath.dataPath = 'blabla/chronic_ECoG/'; % RESPect database for the ECoGs
+                % DTI
+                localDataPath.DWIpath = 'blabla/derivatives/preprocess_DWI/' ; % path to the processed DWI's folder
+                localDataPath.coreg_ROIpath = 'blabla/derivatives/coreg_ROI/'; % path to the designated co-registration and roi definition folder ('coreg_ROI') 
+                localDataPath.DWIMATLABpath = 'blabla/derivatives/dwi_matlab/'; % path to designated folder 'dwi_matlab' to save computed variables in matlab for further use in R/mrtrix 
+                localDataPath.FTpath = '/blabla/derivatives/tract2connectome_all/'; % path to the designated fiber tractography folder ('tracts2connectome_all')
             end
         end
     end
