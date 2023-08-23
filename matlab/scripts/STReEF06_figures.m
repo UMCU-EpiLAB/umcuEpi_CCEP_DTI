2b4c2a1ffe7317d7751406f5cf0bac3e3fee745f
Susanne Jelsma <susanne@bird.umcutrecht.nl>
2023-08-23 15:25:24 +0200

    make all figures with a function

--- matlab/scripts/STReEF06_figures.m
+++ matlab/scripts/STReEF06_figures.m
@@ -4,101 +4,93 @@
 % author: Susanne Jelsma & Dorien van Blooijs
 % date: June 2022
 
+%% SECTION 1: prepare data 
+
+%% set paths
+% set umcuEpi_CCEP_DTI/matlab in your directory and run this section
+
+clc
+clear
+cfg.folderinput = 'chronic_ECoG'; % from which folder would you like to load ECoGs?
+myDataPath = setLocalDataPath(cfg);
+
+%% patient characteristics
+%search for the SPES patients who have a DWI scan
+
+files = dir(myDataPath.DWIpath);
+files(1:2) = [];
+sub_label = cell(1,size(files,1));
+for subj=1:size(files,1)
+    sub_label(1,subj)= cellstr(erase(files(subj).name,'sub-'));
+end
+
+i = 1;
+for subj= 1:size(sub_label,2)
+    x = input(sprintf('Load subject %s? (y/n): ',char(sub_label(subj))),'s');       
+    if strcmp(x,'y') 
+        cfg.sub_label(1,i) = sub_label(subj) ;
+        i=i+1;
+    end
+end
+
+clear sub_label files i x subj
+
+cfg = selectPatients(cfg, myDataPath);
+
+%% load processed network data(see STReEF05_compare_networks) electrode contact area data (see STReEF02_coreg_roidef_mrtrix) and visual scored CCEP data (see umcuEpi_CCEP_DTI (main)/matlab/scripts/ccepDTI03_detectCCEP.m for scoring CCEP data)
+
+dataBase = load_derivatives_data(myDataPath,cfg);
+
+disp('network data loaded')
+
+%% merge runs 
+% Be aware! Code with potential of causing errors due to copying of information, so check for new types of patients or new code added!
+% implement in function
+for subj= 1:size(dataBase,2)
+    if size(dataBase(subj).ccep,2) > 1
+       dataBase(subj).ccep_runs = dataBase(subj).ccep; % copy everything to ccep_runs
+       data = dataBase(subj).ccep(1).ccep; % start with the basic info from the first run
+       dataBase(subj).ccep = data;
+       dataBase(subj).ccep.run_label = dataBase(subj).ccep_runs(1).run_label;
+            for run = 2:size(cfg.run_label{subj},2)
+                n1_peak_sample = dataBase(subj).ccep_runs(run).ccep.n1_peak_sample;
+                n1_peak_amplitude = dataBase(subj).ccep_runs(run).ccep.n1_peak_amplitude;
+                scored = dataBase(subj).ccep_runs(run).ccep.checked;
+                stimsets = dataBase(subj).ccep_runs(run).ccep.cc_stimsets;
+                stimchannels = dataBase(subj).ccep_runs(run).ccep.cc_stimchans;
+                run_label = dataBase(subj).ccep_runs(run).run_label;                  
+    
+                dataBase(subj).ccep.run_label = [dataBase(subj).ccep.run_label run_label];
+                dataBase(subj).ccep.checked = [dataBase(subj).ccep.checked scored];
+                dataBase(subj).ccep.n1_peak_sample = [dataBase(subj).ccep.n1_peak_sample n1_peak_sample];
+                dataBase(subj).ccep.n1_peak_amplitude = [dataBase(subj).ccep.n1_peak_amplitude n1_peak_amplitude];
+                dataBase(subj).ccep.cc_stimsets = [dataBase(subj).ccep.cc_stimsets;stimsets];
+                dataBase(subj).ccep.cc_stimchans = [dataBase(subj).ccep.cc_stimchans;stimchannels];
+            end
+        fprintf('...Runs subject %s has been merged ... \n',dataBase(subj).sub_label)
+    else
+       data = dataBase(subj).ccep.ccep; % if subject contains only one run
+       run_label = dataBase(subj).ccep.run_label;
+       dataBase(subj).ccep = data;
+       dataBase(subj).ccep.run_label = run_label;
+    end
+end
+
+disp('runs merged')
+clear scored run subj stimsets stimchannels  n1_peak_sample n1_peak_amplitude
+
+%% SECTION 2  
 % visualize the inter-modal similarity with connectivity matrices and visualize the network topography per patient
 
 %% visualize the inter-modal similarity with connectivity matrices
 
 for subj = 1:size(dataBase,2)
-V = visual_networks(dataBase(subj).dwi.SC_matrix,dataBase(subj).ccep.EC_matrix,dataBase(subj).sub_label) % function to plot the connectivity matrices
+V = visual_networks(dataBase(subj).network.SC_matrix,dataBase(subj).network.EC_matrix,dataBase(subj).sub_label) % function to plot the connectivity matrices
 V.WindowState = 'maximized';
 print('-vector','-depsc',V,sprintf('visual_networks_symetric%s',dataBase(subj).sub_label))% save the figure for further processing with Adobe Illustrator
 end
 %%  visualize the network topography per patient (must become a function)
-% example of a plot with degree
-markers = ['o';'+';'*';'.';'x';'s';'d';'p';'^';'v';'>';'<';'_'];
-d_SC = [];d_EC = [];bc_SC = [];bc_EC = [];cc_SC = [];cc_EC = [];
-i=0;j=8; % j=8
-set(gcf,'renderer','Painters')
-f4 = figure(5);
-f4.WindowState = 'maximized';
-
 for subj = 1:size(dataBase,2)
-    i=i+1;j=j+1;
-sub_label = erase(dataBase(subj).sub_label,'sub-');
-SC = dataBase(subj).dwi.SC_matrix;
-EC =  dataBase(subj).ccep.EC_matrix;  
-
-ch = dataBase(subj).ccep.ch;
-elec_include = dataBase(subj).dwi.elec_include;
-ch_include = ch(elec_include);
-soz_ind = dataBase(subj).soz_ind;
-no_soz_ind = dataBase(subj).no_soz_ind;
-
-% compute the DEGREE
-G_SC = graph(SC,ch_include); % graph structure
-G_EC = graph(EC,ch_include);
-
-degree_SC = degree(G_SC);
-degree_EC = degree(G_EC);
-degree_SC_soz = degree_SC(soz_ind);
-degree_EC_soz = degree_EC(soz_ind);
-degree_SC_nsoz = degree_SC(no_soz_ind);
-degree_EC_nsoz = degree_EC(no_soz_ind);
-
- 
-nc_EC = degree_EC_soz;nc_SC = degree_SC_soz;
-nnc_EC = degree_EC_nsoz;nnc_SC = degree_SC_nsoz;
-
-subplot(4,2,i)
-
-ns = scatter(nnc_EC,nnc_SC,60,'LineWidth', 1.8); %200 80
-color = 'k'; %[0.7,0.7,0.7];
-ns.MarkerEdgeColor = color;
-ns.Marker = markers(subj);
-hold on
-s = scatter(nc_EC,nc_SC,60,'LineWidth', 1.8); 
-color = [0.4940 0.1840 0.5560];
-s.MarkerEdgeColor = color;
-s.Marker = markers(subj);
-
-yt = get(gca, 'YTick');
-xt = get(gca, 'XTick');
-xlim([0,round(max(degree_EC)/5)*5])
-set(gca, 'XTick',[min(xt),round(max(degree_EC/2)/5)*5,round(max(degree_EC)/5)*5])%numelxt
-set(gca, 'XTickLabel',[min(xt),round(max(degree_EC/2)/5)*5,round(max(degree_EC)/5)*5])%numelxt
-
-set(gca, 'YTick',[min(yt),round(max(degree_SC/2)/5)*5,round(max(degree_SC)/5)*5])
-set(gca, 'YTickLabel',[min(yt),round(max(degree_SC/2)/5)*5,round(max(degree_SC)/5)*5])
-
-[P,S] = polyfit(degree_EC,degree_SC,1);
-[y_fit, delta] = polyval(P,degree_EC,S);
-
-[RHO,PVAL] = corr(degree_SC,degree_EC,'Type','Spearman'); % compute the correlation between the degrees
-% make the p-values presentable in a figure
-if PVAL < 0.0001
-    PVAL = '< 0.0001';
-elseif PVAL < 0.001
-    PVAL = '< 0.001';
-elseif PVAL < 0.01
-    PVAL = '< 0.01';
-elseif PVAL < 0.05
-    PVAL = '< 0.05';
-else 
-    PVAL = sprintf('= %g',round(PVAL,2));
+V = visual_topography(dataBase(subj).network.degree_EC, dataBase(subj).network.degree_SC, dataBase(subj).network.degree_EC_soz, dataBase(subj).network.degree_EC_nsoz,dataBase(subj).network.degree_SC_soz, dataBase(subj).network.degree_SC_nsoz,subj)
+saveas(V,'correlation degree all','epsc') % save the figure for further processing with Adobe Illustrator
 end
-            
-hold on
-color = [0.99 0.38 0.22];
-
-% plot polyfit throught data points
-plot(degree_EC,y_fit,'Color',color,'LineWidth',2)
-set(gca, 'FontSize',12)
-
-if ismember(subj,[1,6,8,9,10])
-    title(sprintf('%s Degree patient %g, p %s , r_{s}= %g','ECoG',j,PVAL,round(RHO,2)))
-else
-title(sprintf('%s Degree patient %g, p %s , r_{s}= %g','sEEG',j,PVAL,round(RHO,2)))
-end
-end
-
-saveas(f4,'correlation degree all','epsc') % save the figure for further processing with Adobe Illustrator
