aad0a7a16991502e4c826e74ee1929c557304e26
Susanne Jelsma <susanne@blue.umcutrecht.nl>
2024-08-06 11:34:01 +0200

    include ratio structural and effective connections that did not overlap

--- matlab/functions/visual_topography.m
+++ matlab/functions/visual_topography.m
@@ -15,27 +15,25 @@ function V = visual_topography(vector_v1, vector_v2, v1_soz, v1_nsoz, v2_soz, v2
 % 
 % Visualize the correlation between a topography measure from structural and effective networks
 
-markers = ['o';'+';'*';'.';'x';'s';'d';'p';'^';'v';'>';'<';'_'];
+marker = '.'; 
 
 set(gcf,'renderer','Painters')
-V = figure(2);
+V = figure(1);
 V.WindowState = 'maximized';
  
- subplot(2,2,i)
+subplot(2,3,i)
 
 
-ns = scatter(v1_nsoz,v2_nsoz,60,'LineWidth', 1.8); 
+ns = scatter(v1_nsoz,v2_nsoz,400,'LineWidth', 1.8); 
 color = 'k'; %[0.7,0.7,0.7];
 ns.MarkerEdgeColor = color;
-ns.Marker = markers(subj);
+ns.Marker = marker; 
 hold on
-s = scatter(v1_soz,v2_soz,60,'LineWidth', 1.8); 
-color = [0.4940 0.1840 0.5560];
+s = scatter(v1_soz,v2_soz,400,'LineWidth', 1.8); 
+color = [204/250 37/250 41/250]; %red [0.24 0.59 0.32] ;%GREEN %[0.4940 0.1840 0.5560];
 s.MarkerEdgeColor = color;
-s.Marker = markers(subj);
+s.Marker = marker; 
 
-yt = get(gca, 'YTick');
-xt = get(gca, 'XTick');
 xlim([0,round(max([vector_v1;vector_v2])/5)*5])
 ylim([0,round(max([vector_v1;vector_v2])/5)*5])
 
@@ -45,19 +43,13 @@ set(gca, 'XTickLabel',[0,round(max([vector_v1;vector_v2]/2)/5)*5,round(max([vect
 set(gca, 'YTick',[0,round(max([vector_v1;vector_v2]/2)/5)*5,round(max([vector_v1;vector_v2])/5)*5])
 set(gca, 'YTickLabel',[0,round(max([vector_v1;vector_v2]/2)/5)*5,round(max([vector_v1;vector_v2])/5)*5])
 
-% xlim([0,round(max(vector_v1)/5)*5])
-% 
-% set(gca, 'XTick',[min(xt),round(max(vector_v1/2)/5)*5,round(max(vector_v1)/5)*5])%numelxt
-% set(gca, 'XTickLabel',[min(xt),round(max(vector_v1/2)/5)*5,round(max(vector_v1)/5)*5])%numelxt
-% 
-% set(gca, 'YTick',[min(yt),round(max(vector_v2/2)/5)*5,round(max(vector_v2)/5)*5])
-% set(gca, 'YTickLabel',[min(yt),round(max(vector_v2/2)/5)*5,round(max(vector_v2)/5)*5])
 axis square
 
 [P,S] = polyfit(vector_v1,vector_v2,1);
-[y_fit, delta] = polyval(P,vector_v1,S);
+[y_fit,~] = polyval(P,vector_v1,S);
 
 [RHO,PVAL] = corr(vector_v2,vector_v1,'Type','Spearman'); % compute the correlation between the degrees
+
 % make the p-values presentable in a figure
 if PVAL < 0.0001
     PVAL = '< 0.0001';
@@ -75,13 +67,9 @@ hold on
 color = [0.99 0.38 0.22];
 
 % plot polyfit throught data points
-plot(vector_v1,y_fit,'Color',color,'LineWidth',2)
-set(gca, 'FontSize',12) %12
+plot(vector_v1,y_fit,'Color',color,'LineWidth',2.8)
+set(gca, 'FontSize',16) %12
 
 title(sprintf('p %s , r_{s}= %g', PVAL,round(RHO,2)))
 
-% if ismember(subj,[1,6,8,9,10])
-%     title(sprintf('%s Degree patient %g, p %s , r_{s}= %g','ECoG',i,PVAL,round(RHO,2)))
-% else
-% title(sprintf('%s Degree patient %g, p %s , r_{s}= %g','sEEG',i,PVAL,round(RHO,2)))
 end
