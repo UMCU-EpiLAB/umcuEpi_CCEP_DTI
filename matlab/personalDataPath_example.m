41e4c7f798612ab830d812b1cd0088c016202832
Susanne Jelsma <susanne@blue.umcutrecht.nl>
2024-08-18 22:52:15 +0200

    personalDataPath example

--- matlab/personalDataPath_example.m
+++ matlab/personalDataPath_example.m
@@ -8,12 +8,26 @@ function localDataPath = personalDataPath_example(varargin)
 
 if ~isempty(varargin{1})
     if isstruct(varargin{1})
-        if sum(contains(fieldnames(varargin{1}),'folderinput'))              
-                localDataPath.dataPath = '/blabla/derivatives/STReEF/DATA_for_publication/'; % main data to perform calculations and make figures of manuscript
-        end
+        if sum(contains(fieldnames(varargin{1}),'folderinput'))  
+            if contains(varargin{1}.folderinput,'shareData_STReEF')
+            localDataPath.input = '/blabla/STReEF/shareData_STReEF/'; % main effective BIDS data
+            localDataPath.input_dev = '/blabla/shareData_STReEF/derivatives/'; % derivatives of the sub scripts, used to run each script on is own  
+
+            % set paths output data
+            localDataPath.output = '/blabla/shareData_STReEF/derivatives/'; % save derivatives of the sub scripts    
+          
+           end
+        end   
     end
 end
-end
+
+%  set paths fieldtrip 
+fieldtrip_folder  = '/blabla/fieldtrip/';
+%  copy the private folder in fieldtrip to somewhere else
+fieldtrip_private = '/blabla/fieldtrip_private/';
+addpath(fieldtrip_folder)
+addpath(fieldtrip_private)
+ft_defaults
 
 
 end
